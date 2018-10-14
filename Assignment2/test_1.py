aeb8e345f8277e696316b9c1f8afd30c34e96d74
M Arun kumar <arunmiryala@ee.iitb.ac.in>
2018-10-14 18:56:56 +0530

    tested for several images

--- Assignment2/test_1.py
+++ Assignment2/test_1.py
@@ -1,10 +1,19 @@
 import numpy as np
 import cv2 as cv
-
-kernel = cv.imread("kernel.png",0)
+import matplotlib.pyplot as plt
+kernel = cv.imread("kernel/kernel.png",0)
+kernel = kernel.astype(np.float)
+img = cv.imread("ground_truth/GroundTruth1_1_1.jpg",0)
+kernel = np.pad(kernel,[(0,800-21),(0,800-21)],"constant")/np.sum(kernel)
+plt.imshow(kernel)
 f = np.fft.fft2(kernel)
+img_f = np.fft.fft2(img)
 fshift = np.fft.fftshift(f)
-magnitude_spectrum = 20*np.log(np.abs(fshift))
+img_sh = np.fft.fftshift(img_f)
+# np.fft.ifft2(fshift*img_sh)
+
+# magnitude_spectrum = 20*np.log(np.abs(fshift))
 # self.__mdfd_img = magnitude_spectrum # now move the data in temp variable to global variable
-cv.imshow("dummy",magnitude_spectrum)
-cv.waitKey(0)
+# plt.imshow(np.real(np.fft.ifftshift(np.fft.ifft2(fshift*img_sh))))
+# cv.waitKey(0)
+plt.show()
